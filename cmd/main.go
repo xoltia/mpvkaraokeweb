40fbd230e2e6ae8948f08d7cd9e15ab8b2ddc0b3
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2024-03-16 13:21:34 -0500

    Add gzip compression

--- cmd/main.go
+++ cmd/main.go
@@ -14,20 +14,22 @@ import (
 	"strings"
 	"time"
 
+	"github.com/NYTimes/gziphandler"
 	"github.com/UTD-JLA/mpvwebkaraoke"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/wader/goutubedl"
 )
 
 var (
-	addr         = flag.String("addr", ":8080", "address to listen on")
-	dbPath       = flag.String("db", "karaoke.sqlite", "path to sqlite database")
-	cachePath    = flag.String("cache", "vidcache", "path to video cache")
-	disableCache = flag.Bool("disable-cache", false, "disable video cache")
-	ytdlPath     = flag.String("ytdl", "yt-dlp", "path to youtube-dl")
-	ytdlFilter   = flag.String("ytdl-filter", "bestvideo[ext=mp4][height<=1080]+bestaudio/best", "youtube-dl filter")
-	adminCode    = flag.String("admin-code", "", "code needed for registering as admin")
-	maxUserQueue = flag.Int("max-queue", 1, "maximum number of songs a user can queue")
+	addr          = flag.String("addr", ":8080", "address to listen on")
+	dbPath        = flag.String("db", "karaoke.sqlite", "path to sqlite database")
+	cachePath     = flag.String("cache", "vidcache", "path to video cache")
+	disableCache  = flag.Bool("disable-cache", false, "disable video cache")
+	ytdlPath      = flag.String("ytdl", "yt-dlp", "path to youtube-dl")
+	ytdlFilter    = flag.String("ytdl-filter", "bestvideo[ext=mp4][height<=1080]+bestaudio/best", "youtube-dl filter")
+	adminCode     = flag.String("admin-code", "", "code needed for registering as admin")
+	maxUserQueue  = flag.Int("max-queue", 1, "maximum number of songs a user can queue")
+	noCompression = flag.Bool("no-compression", false, "disable gzip compression")
 )
 
 // wraps with newlines if text is too long
@@ -161,17 +163,32 @@ func main() {
 		http.Redirect(w, r, "/queue", http.StatusFound)
 	})
 
-	mux.HandleFunc("GET /register", authHandler.HandleIndex)
-	mux.HandleFunc("POST /register", authHandler.HandleRegister)
-	mux.HandleFunc("GET /queue", authHandler.Wrap(queueHandler.HandleIndex))
-	mux.HandleFunc("GET /queue/request", authHandler.Wrap(queueHandler.HandleSubmissionPage))
-	mux.HandleFunc("POST /queue/preview", authHandler.Wrap(queueHandler.HandlePostPreview))
-	mux.HandleFunc("POST /queue/request", authHandler.Wrap(queueHandler.HandlePostSubmission))
-	mux.HandleFunc("DELETE /queue/revoke/{id}", authHandler.Wrap(queueHandler.HandleRevoke))
-	mux.HandleFunc("GET /queue/current", authHandler.Wrap(queueHandler.HandleCurrentSong))
-	mux.HandleFunc("GET /queue/members", authHandler.Wrap(queueHandler.HandleMemberList))
-	mux.HandleFunc("GET /sse", authHandler.Wrap(queueHandler.HandleSSE))
+	if *noCompression {
+		mux.HandleFunc("GET /register", authHandler.HandleIndex)
+		mux.HandleFunc("POST /register", authHandler.HandleRegister)
+		mux.HandleFunc("GET /queue", authHandler.Wrap(queueHandler.HandleIndex))
+		mux.HandleFunc("GET /queue/request", authHandler.Wrap(queueHandler.HandleSubmissionPage))
+		mux.HandleFunc("POST /queue/preview", authHandler.Wrap(queueHandler.HandlePostPreview))
+		mux.HandleFunc("POST /queue/request", authHandler.Wrap(queueHandler.HandlePostSubmission))
+		mux.HandleFunc("DELETE /queue/revoke/{id}", authHandler.Wrap(queueHandler.HandleRevoke))
+		mux.HandleFunc("GET /queue/current", authHandler.Wrap(queueHandler.HandleCurrentSong))
+		mux.HandleFunc("GET /queue/members", authHandler.Wrap(queueHandler.HandleMemberList))
+		//mux.HandleFunc("GET /sse", authHandler.Wrap(queueHandler.HandleSSE))
+	} else {
+		mux.Handle("GET /register", gziphandler.GzipHandler(http.HandlerFunc(authHandler.HandleIndex)))
+		mux.Handle("POST /register", gziphandler.GzipHandler(http.HandlerFunc(authHandler.HandleRegister)))
+		mux.Handle("GET /queue", gziphandler.GzipHandler(http.HandlerFunc(authHandler.Wrap(queueHandler.HandleIndex))))
+		mux.Handle("GET /queue/request", gziphandler.GzipHandler(http.HandlerFunc(authHandler.Wrap(queueHandler.HandleSubmissionPage))))
+		mux.Handle("POST /queue/preview", gziphandler.GzipHandler(http.HandlerFunc(authHandler.Wrap(queueHandler.HandlePostPreview))))
+		mux.Handle("POST /queue/request", gziphandler.GzipHandler(http.HandlerFunc(authHandler.Wrap(queueHandler.HandlePostSubmission))))
+		mux.Handle("DELETE /queue/revoke/{id}", gziphandler.GzipHandler(http.HandlerFunc(authHandler.Wrap(queueHandler.HandleRevoke))))
+		mux.Handle("GET /queue/current", gziphandler.GzipHandler(http.HandlerFunc(authHandler.Wrap(queueHandler.HandleCurrentSong))))
+		mux.Handle("GET /queue/members", gziphandler.GzipHandler(http.HandlerFunc(authHandler.Wrap(queueHandler.HandleMemberList))))
+		//mux.Handle("GET /sse", gziphandler.GzipHandler(http.HandlerFunc(authHandler.Wrap(queueHandler.HandleSSE))))
+	}
 
+	// TODO: gzip SSE
+	mux.HandleFunc("GET /sse", authHandler.Wrap(queueHandler.HandleSSE))
 	http.Handle("/", mux)
 
 	go loopMPV(queue, vidCache)
